f5bd6335f82d509eed2182f0c13905257101c3f8
basitdevs <arsajid090@gmail.com>
2025-03-11 06:35:57 +0500

    fixing

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
+  subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-inter",
 });
